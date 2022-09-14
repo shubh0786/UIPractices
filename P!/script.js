10ab32b869e09b2b7b156d65bbe058199b957b0b
SHUBHAM SINGH <shubham.singh267@gmail.com>
2022-09-14 13:24:19 +1200

    first commit

---
+++ P!/script.js
@@ -0,0 +1,8 @@
+var index = 0;
+function changeColors(){
+    var colors =["red","blue","organge","yellow","green","purple"];
+    document.getElementsByTagName("body")[0].style.background = colors[index++];
+    if(index > colors.length - 1){
+        index = 0;
+    }
+}
