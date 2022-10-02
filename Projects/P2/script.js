af3709abbf2c6ca798f4af96aa06b0637cdbbb3a
SHUBHAM SINGH <shubham.singh267@gmail.com>
2022-10-02 23:57:17 +1300

    save

--- Projects/P2/script.js
+++ Projects/P2/script.js
@@ -1,6 +1,6 @@
 var index = 0;
 function changeColors(){
-    var colors =["red","blue","organge","yellow","green","purple"];
+    var colors =["red","blue","organge","yellow","green","purple","black","white"];
     document.getElementsByTagName("body")[0].style.background = colors[index++];
     if(index > colors.length - 1){
         index = 0;
